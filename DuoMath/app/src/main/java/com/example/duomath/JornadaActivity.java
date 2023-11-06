8c34ff08793b6ee9a02248b3c649d00e2314dcbc
IPreHandaI <joao.ggotardo@gmail.com>
2023-11-06 09:08:43 -0300

    att

---
+++ DuoMath/app/src/main/java/com/example/duomath/JornadaActivity.java
@@ -0,0 +1,14 @@
+package com.example.duomath;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class JornadaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_jornada);
+    }
+}
