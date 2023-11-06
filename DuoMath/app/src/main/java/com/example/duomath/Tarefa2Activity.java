c906bf3d59bf19341cc8c26aa92d6f2300a25e4c
IPreHandaI <joao.ggotardo@gmail.com>
2023-11-06 10:02:26 -0300

    att

---
+++ DuoMath/app/src/main/java/com/example/duomath/Tarefa2Activity.java
@@ -0,0 +1,14 @@
+package com.example.duomath;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Tarefa2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tarefa2);
+    }
+}
