6c1b3f7e617e83d7e000e598e00e18f0dd1d7d06
JoaoGGotardo <joao.ggotardo@gmail.com>
2023-11-07 08:46:16 -0300

    att

--- DuoMath/app/src/main/java/com/example/duomath/MainActivity.java
+++ DuoMath/app/src/main/java/com/example/duomath/MainActivity.java
@@ -2,14 +2,28 @@ package com.example.duomath;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnJourney;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnJourney = findViewById(R.id.btnSelectJourney);
+        btnJourney.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent telaJornada = new Intent(getApplicationContext(),JornadaActivity.class);
+                startActivity(telaJornada);
+            }
+        });
 
     }
 }
--- DuoMath/app/src/main/java/com/example/duomath/Tarefa1Activity.java
+++ DuoMath/app/src/main/java/com/example/duomath/Tarefa1Activity.java
@@ -3,12 +3,22 @@ package com.example.duomath;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+import java.util.Random;
 
 public class Tarefa1Activity extends AppCompatActivity {
+
+    Button r1, r2, r3, r4, btnVoltar;
+    TextView txtTermo1, txtTermo2, txtOp, txtResp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tarefa1);
+
+        
+
+
     }
 }
