539777180df334df8ff3f05835732d3d4ee82d64
JoaoGGotardo <joao.ggotardo@gmail.com>
2023-11-07 10:16:34 -0300

    att

--- DuoMath/app/src/main/java/com/example/duomath/JornadaActivity.java
+++ DuoMath/app/src/main/java/com/example/duomath/JornadaActivity.java
@@ -2,13 +2,27 @@ package com.example.duomath;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class JornadaActivity extends AppCompatActivity {
+
+    Button btnBasico, btnVoltar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_jornada);
+
+        btnBasico = findViewById(R.id.btnBasico);
+        btnBasico.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent telaMain = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(telaMain);
+            }
+        });
     }
 }
--- DuoMath/app/src/main/java/com/example/duomath/Tarefa1Activity.java
+++ DuoMath/app/src/main/java/com/example/duomath/Tarefa1Activity.java
@@ -2,7 +2,9 @@ package com.example.duomath;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import java.util.Random;
@@ -27,11 +29,7 @@ public class Tarefa1Activity extends AppCompatActivity {
         txtOp = findViewById(R.id.txtOp);
         txtResp = findViewById(R.id.txtResp);
 
-
-
-
-
-        Integer questionType = new Random().nextInt(4);
+        Integer questionType = new Random().nextInt(1);
 
         switch(questionType){
             case 0:
@@ -41,7 +39,11 @@ public class Tarefa1Activity extends AppCompatActivity {
 
                 Integer[] alternativas = {resposta - 1, resposta + 1, resposta, resposta, resposta + 2};
 
-                
+                txtTermo1.setText(Integer.toString(termo1));
+                txtTermo2.setText(Integer.toString(termo2));
+                txtResp.setText(" = X");
+                txtOp.setText(" + ");
+
 
 
                 break;
@@ -55,6 +57,15 @@ public class Tarefa1Activity extends AppCompatActivity {
 
                 break;
         }
+
+        btnVoltar = findViewById(R.id.btnVoltar);
+        btnVoltar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent telaJornada = new Intent(getApplicationContext(),JornadaActivity.class);
+                startActivity(telaJornada);
+            }
+        });
 
 
     }
