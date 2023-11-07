6ebcd894d3e89c2005754b19d7a4d75b0d8e250a
JoaoGGotardo <joao.ggotardo@gmail.com>
2023-11-07 09:40:32 -0300

    att

--- DuoMath/app/src/main/java/com/example/duomath/Tarefa1Activity.java
+++ DuoMath/app/src/main/java/com/example/duomath/Tarefa1Activity.java
@@ -17,7 +17,44 @@ public class Tarefa1Activity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tarefa1);
 
-        
+        r1 = findViewById(R.id.resp1);
+        r2 = findViewById(R.id.resp2);
+        r3 = findViewById(R.id.resp3);
+        r4 = findViewById(R.id.resp4);
+
+        txtTermo1 = findViewById(R.id.txtTermo1);
+        txtTermo2 = findViewById(R.id.txtTermo2);
+        txtOp = findViewById(R.id.txtOp);
+        txtResp = findViewById(R.id.txtResp);
+
+
+
+
+
+        Integer questionType = new Random().nextInt(4);
+
+        switch(questionType){
+            case 0:
+                Integer termo1 = new Random().nextInt(26);
+                Integer termo2 = new Random().nextInt(26);
+                Integer resposta = termo1 + termo2;
+
+                Integer[] alternativas = {resposta - 1, resposta + 1, resposta, resposta, resposta + 2};
+
+                
+
+
+                break;
+            case 1:
+
+                break;
+            case 2:
+
+                break;
+            case 3:
+
+                break;
+        }
 
 
     }
